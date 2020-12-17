aa01a275ef75e4bffc5b4cb547f80f71498aae0e
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-12-17 20:14:45 +0100

    Fixed types

--- src/mwc-menu.ts
+++ src/mwc-menu.ts
@@ -1,10 +1,12 @@
 import { MenuBase } from '@material/mwc-menu/mwc-menu-base';
+import type { Menu as M } from '@material/mwc-menu';
 import { style } from '@material/mwc-menu/mwc-menu-css';
 import { MenuSurface } from '@material/mwc-menu/mwc-menu-surface';
 import { Scoped } from 'scoped-elements';
 import { List } from './mwc-list';
+import { Constructor } from 'lit-element';
 
-export class Menu extends Scoped(class extends MenuBase {}) {
+export class Menu extends Scoped((MenuBase as any) as Constructor<M>) {
   static get scopedElements() {
     return {
       'mwc-menu-surface': MenuSurface,
@@ -13,5 +15,3 @@ export class Menu extends Scoped(class extends MenuBase {}) {
   }
   static styles = style;
 }
-
-
