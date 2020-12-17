e6d2f99937f9489fbf45975afd0c900f99594561
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-12-17 13:55:10 +0100

    Setup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,20 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
+import multiInput from 'rollup-plugin-multi-input';
 
 const pkg = require('./package.json');
 
 export default {
-  input: `src/index.ts`,
-  output: [{ dir: './', format: 'es', sourcemap: true }],
+  input: ['src/**/*.ts'],
+  output: [{ dir: 'dist', format: 'es', sourcemap: true }],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash-es')
-  external: [],
+  external: [...Object.keys(pkg).filter(key => !key.startsWith('mwc'))],
   watch: {
     include: 'src/**',
   },
   plugins: [
+    multiInput(),
     replace({
       'window.customElements.define(tagName, clazz);': '',
       delimiters: ['', ''],
