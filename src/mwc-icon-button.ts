5489a6dbb2ce58d65e18a87e28804a1a9b161475
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-12-28 21:30:59 +0100

    Fixed icon button

--- src/mwc-icon-button.ts
+++ src/mwc-icon-button.ts
@@ -2,7 +2,7 @@ import { IconButtonBase } from '@material/mwc-icon-button/mwc-icon-button-base';
 import { style } from '@material/mwc-icon-button/mwc-icon-button-css';
 import { ScopedElementsMixin as Scoped } from '@open-wc/scoped-elements';
 import { ScopedElementsHost } from '@open-wc/scoped-elements/types/src/types';
-import { Constructor } from 'lit-element';
+import { Constructor, html, queryAsync } from 'lit-element';
 import { Ripple } from './mwc-ripple';
 
 export class IconButton extends (Scoped(IconButtonBase) as Constructor<
@@ -14,4 +14,13 @@ export class IconButton extends (Scoped(IconButtonBase) as Constructor<
       'mwc-ripple': Ripple,
     };
   }
+
+  renderRipple() {
+    return this.shouldRenderRipple
+      ? html` <mwc-ripple class="ripple" .disabled="${this.disabled}" unbounded>
+        </mwc-ripple>`
+      : '';
+  }
+  @queryAsync('.ripple')
+  ripple!: Promise<Ripple | null>;
 }
