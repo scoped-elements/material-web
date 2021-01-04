f8ccfdbe0ffcb2cc893b574238e3df06cbc07a7f
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-01-04 11:57:07 +0100

    Fixed snackbar build

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,8 @@ export default {
   output: [{ dir: 'dist', format: 'es', sourcemap: true }],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash-es')
   external(id) {
-    if (!id.includes('mwc-')) return true;
+    if (id.includes('snackbar')) console.log(id);
+    if (!id.includes('mwc-') && !id.includes('./')) return true;
     if (id.includes('mwc-form') || id.includes('mwc-base')) return true;
     return false;
   },
