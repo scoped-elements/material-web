a10c1e4e3ffabf4e4899274228cdfcb8ece64df6
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-10-10 13:15:11 +0200

    Fixed card styles

--- src/mwc-card.ts
+++ src/mwc-card.ts
@@ -1,2 +1,16 @@
 import CardBase from '@ui5/webcomponents/dist/Card.js';
-export class Card extends CardBase {}
+import { css } from 'lit';
+import cardCss from '@ui5/webcomponents/dist/generated/themes/Card.css.js';
+
+export class Card extends CardBase {
+  static get styles() {
+    return [
+      cardCss,
+      css`
+        :host {
+          display: flex !important;
+        }
+      `,
+    ];
+  }
+}
