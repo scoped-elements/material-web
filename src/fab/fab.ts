b0d4942c7bf215f31f0445f25bc225b000ab029d
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-02-16 15:45:30 +0100

    Fix fab

--- src/fab/fab.ts
+++ src/fab/fab.ts
@@ -6,12 +6,15 @@
 
 import { MdIcon } from '../icon/icon.js';
 
-import {html, TemplateResult} from 'lit';
+import { html, TemplateResult } from 'lit';
 
-import {Fab} from '@material/web/fab/lib/fab.js';
-import {styles as sharedStyles} from '@material/web/fab/lib/fab-shared-styles.css.js';
-import {styles as fabStyles} from '@material/web/fab/lib/fab-styles.css.js';
+import { Fab } from '@material/web/fab/lib/fab.js';
+import { styles as sharedStyles } from '@material/web/fab/lib/fab-shared-styles.css.js';
+import { styles as fabStyles } from '@material/web/fab/lib/fab-styles.css.js';
 import { ScopedElementsMixin } from '@open-wc/scoped-elements';
+import { MdElevation } from '../elevation/elevation.js';
+import { MdFocusRing } from '../focus/focus-ring.js';
+import { MdRipple } from '../ripple/ripple.js';
 declare global {
   interface HTMLElementTagNameMap {
     'md-fab': MdFab;
@@ -28,13 +31,16 @@ export class MdFab extends ScopedElementsMixin(Fab) {
   static override styles = [sharedStyles, fabStyles];
 
   /** @soyTemplate */
-  protected override renderIcon(icon: string): TemplateResult|string {
+  protected override renderIcon(icon: string): TemplateResult | string {
     return icon ? html`<md-icon class="md3-fab__icon">${icon}</md-icon>` : '';
   }
-  
+
   static get scopedElements() {
     return {
       'md-icon': MdIcon,
+      'md-elevation': MdElevation,
+      'md-focus-ring': MdFocusRing,
+      'md-ripple': MdRipple,
     };
   }
 }
